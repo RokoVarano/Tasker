e95b00a48b2788fde6de37652d7ca016ffd8bcd7
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-04-08 16:42:27 -0400

    create login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    user_params = params.require(:user).permit(:name)
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
+
+  def show; end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :sessions
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+
 end
