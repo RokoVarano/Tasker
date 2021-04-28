4eb861ce4b13167bb9aa60421795360b556e4a1c
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-04-28 13:56:51 -0400

    Style tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,10 +1,11 @@
 module TasksHelper
   def task_link(task)
     html_string = ""
+    html_string << avatar(task.groups[0]) unless task.groups.empty?
     html_string << "<div>"
-    html_string << "<img src='#{url_for(task.groups[0].image)}'/>" unless task.groups.empty?
-    html_string << "<p>'#{task.name}'</p>"
-    html_string << "<p>'#{task.created_at}'</p>"
+    html_string << "<p>'#{task[:name]}'</p>"
+    html_string << "<p>'#{task[:created_at]}'</p>"
+    html_string << "<p>Points: #{task[:points]}</p>"
     html_string << "</div>"
 
     html_string.html_safe
