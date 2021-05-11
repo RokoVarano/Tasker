609f195f8fa07a9585cfc406d6aa7ea62b3cbeeb
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-11 11:59:07 -0400

    Add task to group on creation, order tasks#index by date

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,9 @@ class TasksController < ApplicationController
     external = params[:external]
 
     @tasks = if external == 'true'
-               Task.where(user_id: current_user).order(:created_at).select { |task| task.groups.empty? }
+               Task.where(user_id: current_user).select { |task| task.groups.empty? }
              else
-               Task.where(user_id: current_user).order(:created_at)
+               Task.where(user_id: current_user)
              end
 
     @title = 'Tasks'
@@ -21,9 +21,15 @@ class TasksController < ApplicationController
   def create
     task_params = params.require(:task).permit(:name, :points, :group)
     @task = current_user.tasks.create(task_params)
-    puts "CURRENT USER: #{current_user[:name]}, ID => #{current_user[:id]}"
-    puts "Task: NAME => #{@task[:name]}, POINTS => #{@task[:points]}, USER => #{@task[:user_id]}"
+
     if @task.save!
+
+      unless params[:group].empty?
+        puts "EL UNLESS DEL GROUPTASK: #{params[:group][:group_id]}"
+        @grouptask = @task.group_tasks.create(:group_id => params[:group][:group_id])
+        @grouptask.save
+      end
+
       redirect_to tasks_path, notice: 'Created New Task!'
     else
       render 'new'
