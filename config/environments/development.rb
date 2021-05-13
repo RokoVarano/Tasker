2ab318768bb2fdf865a43b6fdb6557b67360f9f1
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-13 11:20:58 -0400

    Disable Bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,13 +2,15 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
+    Bullet.enable        = false
     Bullet.alert         = true
     Bullet.bullet_logger = true
     Bullet.console       = true
   # Bullet.growl         = true
     Bullet.rails_logger  = true
     Bullet.add_footer    = true
+
+    Bullet.add_whitelist :type => :unused_eager_loading, :class_name => "Task", :association => :groups
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
