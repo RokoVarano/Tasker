5fc4d2b95620c7835d3b5e627eaaf65a71b53eb5
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-05-14 21:47:03 -0400

    Last touches

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
   def current_user
     return if User.all.empty?
 
-
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
